eaa3f8db4094091e19720dd00732034386da3416
Bulat147 <ninza147258@gmail.com>
2025-03-20 23:36:58 +0300

    add generated files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,26 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Crawler crawler = new CrawlerRealisation("files");
-        crawler.setUrlList(
-                List.of(
-                        "https://lenta.ru/",
-                        "https://ria.ru/lenta/"
-                ));
 
+        List<String> urls = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/urls.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                urls.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+
+        crawler.setUrlList(urls);
         crawler.downloadPages();
     }
 }
